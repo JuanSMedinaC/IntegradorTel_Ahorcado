694f80be868d83af5575bdee64a47bc8b2b9c0d3
unknown <juansemedina30@gmail.com>
2023-08-25 12:42:33 -0500

    partida emparejada

--- src/Client.java
+++ src/Client.java
@@ -5,6 +5,24 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 6000);
+        Runnable matchRecThread=() ->
+        {
+            try{
+                byte [] bf = new byte[500];
+                socket.getInputStream().read(bf);
+                String rec = new String(bf, "UTF-8");
+                System.out.println(rec.trim());
+                socket.close();
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+
+
+        };
+        Thread runReader= new Thread(matchRecThread);
+        runReader.start();
+
 
     }
 }
+
